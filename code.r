9c8ba12db7cf691147b626e411513b23341ee8a3
O-Pain <zrenpz@gmail.com>
2025-09-08 16:18:14 +0200

    Code.r

--- code.r
+++ code.r
@@ -1,1 +1,56 @@
-
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(zoo)
+library(purrr)
+library(car)
+library(lmtest)
+library(sandwich)
+library(corrplot)
+library(forecast)
+library(tseries)
+btc=read.csv("C:/Users/Zrenp/OneDrive/Desktop/Projets/économétrie/Les déterminants des prix des cryptoactifs Assas/Data/dataset.csv")
+fedfunds=read.csv("C:/Users/Zrenp/OneDrive/Desktop/Projets/économétrie/Les déterminants des prix des cryptoactifs Assas/Data/DFF.csv")
+snp=read.csv("C:/Users/Zrenp/OneDrive/Desktop/Projets/économétrie/Les déterminants des prix des cryptoactifs Assas/Data/SP500.csv")
+electricity=read.csv("C:/Users/Zrenp/OneDrive/Desktop/Projets/économétrie/Les déterminants des prix des cryptoactifs Assas/Data/APU000072610.csv")
+btc$Date=ymd(btc$Date)
+fedfunds$Date=ymd(fedfunds$observation_date)
+snp$Date=ymd(snp$observation_date)
+electricity$Date=ymd(electricity$observation_date)
+btc_monthly=btc %>%
+   group_by(YearMonth = floor_date(Date, "month")) %>%
+   summarise(Price_BTC = mean(BTC_Closing, na.rm = TRUE),
+             BTC_volume = mean(BTC_Volume, na.rm = TRUE),
+             FearGreed = mean(Value, na.rm = TRUE))
+ fedfunds_monthly=fedfunds %>%
+   group_by(YearMonth = floor_date(Date, "month")) %>%
+   summarise(Rate = mean(DFF, na.rm = TRUE))
+ snp_monthly=snp %>%
+   group_by(YearMonth = floor_date(Date, "month")) %>%
+   summarise(Price_SP = mean(SP500, na.rm = TRUE))
+ electricity_monthly=electricity %>%
+   group_by(YearMonth = floor_date(Date, "month")) %>%
+   summarise(Consumption = mean(APU000072610, na.rm = TRUE))
+ df_monthly=reduce(list(btc_monthly, fedfunds_monthly, snp_monthly, electricity_monthly),
+                      full_join, by = "YearMonth")
+head(df_monthly)
+str(df_monthly)
+model=lm(Price_BTC ~ BTC_volume + Rate + Price_SP + Consumption + FearGreed,
+             data = df_monthly)
+summary(model)
+vif(model)
+adf.test(df_monthly$Price_BTC)
+adf.test(df_monthly$BTC_volume)
+adf.test(df_monthly$Rate)
+adf.test(df_monthly$Price_SP)
+adf.test(df_monthly$Consumption)
+adf.test(df_monthly$FearGreed)
+dwtest(model)
+bptest(model)
+coeftest(model, vcov = vcovHC(model, type = "HC"))
+cor_matrix=cor(df_monthly[, c("Price_BTC", "BTC_volume", "Rate", "Price_SP", "Consumption", "FearGreed")],
+                   use = "complete.obs")
+corrplot(cor_matrix, method = "color", type = "upper")
+btc_ts=ts(df_monthly$Price_BTC, frequency = 12, start = c(2018,2))
+arima_model=auto.arima(btc_ts)
+summary(arima_model)
